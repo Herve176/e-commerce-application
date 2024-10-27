934eda09781525065ddf7ffa8c23965b1e9e0efe
Herve176 <hdjomo1@gmail.com>
2024-10-27 12:32:34 +0100

    Refactor CSS and HTML in articles and navbar components

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { Output, EventEmitter } from '@angular/core';
-
+import { Article } from '../../models/article';
 @Component({
   selector: 'app-articles',
   standalone: true,
@@ -12,6 +12,7 @@ import { Output, EventEmitter } from '@angular/core';
   styleUrl: './articles.component.css'
 })
 export class ArticlesComponent {
+  Article?:Article;
   value:number=0;
   @Output()Itemadded= new EventEmitter<number>();
 
---
+++ src/models/article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+  id: number;
+  name: String;
+  price:number;
+  description: String;
+  photo: String;
+}
